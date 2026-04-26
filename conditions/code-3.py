301527364fc96d95bfa588d4828860df69d817a0
Ambish21 <ambishyaqoob2005@outlook.com>
2026-04-26 23:45:08 +0500

    add code-3

---
+++ conditions/code-3.py
@@ -0,0 +1,9 @@
+# =========================
+# Code 3: Voting eligibility
+# =========================
+age = int(input("Enter age: "))
+
+if age >= 18:
+    print("Eligible")
+else:
+    print("Not eligible")
