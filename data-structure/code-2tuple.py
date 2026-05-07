5015142756d7f57cbf6abe757cfc0d0d3a4928d3
Ambish21 <ambishyaqoob2005@outlook.com>
2026-05-07 23:57:39 +0500

    add tuple of data structure code

---
+++ data-structure/code-2tuple.py
@@ -0,0 +1,23 @@
+# TUPLES
+
+# creating tuple
+colors = ("red", "green", "blue")
+
+print("Tuple:", colors)
+
+# access element
+print("First Color:", colors[0])
+
+# loop through tuple
+print("All Colors:")
+for color in colors:
+    print(color)
+
+# length
+print("Length:", len(colors))
+
+# count element
+print("Count of red:", colors.count("red"))
+
+# index of element
+print("Index of blue:", colors.index("blue"))
