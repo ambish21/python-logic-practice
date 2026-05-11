1b05dc225d6b2bb42e7b52db8309c0c9877f5366
Ambish21 <ambishyaqoob2005@outlook.com>
2026-05-11 22:54:34 +0500

    code added for dara structure

---
+++ data-structure/code-3dictionary.py
@@ -0,0 +1,31 @@
+# DICTIONARIES
+
+# creating dictionary
+student = {
+    "name": "Ali",
+    "age": 20,
+    "course": "Python"
+}
+
+print("Original Dictionary:", student)
+
+# access value
+print("Name:", student["name"])
+
+# add new item
+student["city"] = "Lahore"
+
+# update value
+student["age"] = 21
+
+print("Updated Dictionary:", student)
+
+# loop through dictionary
+print("Dictionary Data:")
+for key, value in student.items():
+    print(key, ":", value)
+
+# remove item
+student.pop("course")
+
+print("After Remove:", student)
