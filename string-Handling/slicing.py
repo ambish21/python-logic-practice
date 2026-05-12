877e8bae5cc8440d22053ae18f6ae58c693100d9
Ambish21 <ambishyaqoob2005@outlook.com>
2026-05-12 23:29:47 +0500

    String-slicing code is added

---
+++ string-Handling/slicing.py
@@ -0,0 +1,27 @@
+# slicing.py
+
+text = "PythonProgramming"
+
+# First character
+print(text[0])
+
+# Last character
+print(text[-1])
+
+# Characters from index 0 to 5
+print(text[0:6])
+
+# From index 6 to end
+print(text[6:])
+
+# Skip characters
+print(text[::2])
+
+# Reverse string
+print(text[::-1])
+
+# Middle part
+print(text[6:17])
+
+# Last 5 characters
+print(text[-5:])
