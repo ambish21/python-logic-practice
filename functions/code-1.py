963f65bd690e6f864e8afba6d8b909ec31553c63
Ambish21 <ambishyaqoob2005@outlook.com>
2026-05-13 22:15:45 +0500

    update code of function

--- functions/code-1.py
+++ functions/code-1.py
@@ -2,3 +2,17 @@ def greet(name):
     print("Hello", name)
 
 greet("Ambish")
+# defining_functions.py
+
+# Simple function
+def greet():
+    print("Hello, Welcome to Python!")
+
+# Calling function
+greet()
+
+# Another function
+def line():
+    print("----------------------")
+
+line()
