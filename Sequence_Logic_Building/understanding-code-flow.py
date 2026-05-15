1c551f30508eaa2b1585f668b130cb76a83b1509
Ambish21 <ambishyaqoob2005@outlook.com>
2026-05-15 23:27:16 +0500

    understanding code flow code is added

---
+++ Sequence_Logic_Building/understanding-code-flow.py
@@ -0,0 +1,10 @@
+# Step 1: Take input from user
+num = int(input("Enter a number: "))
+
+# Step 2: Use if-elif-else to check conditions
+if num > 0:
+    print("Number is Positive")
+elif num < 0:
+    print("Number is Negative")
+else:
+    print("Number is Zero")
